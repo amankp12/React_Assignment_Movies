bd3f9d55165400c0ed3dbbb7f2e873061d9d8c03
amankp12 <90567114+amankp12@users.noreply.github.com>
2024-03-02 11:31:12 +0530

    first commit

---
+++ src/MovieData.js
@@ -0,0 +1,104 @@
+const MovieData = [
+    {
+        url: 'src/assets/Action/Avatar.jpg',
+        title: 'Avatar'
+    },
+    {
+        url: 'src/assets/Action/Extraction.jpg',
+        title: 'Extraction'
+    },
+    {
+        url: 'src/assets/Action/Troll.jpg',
+        title: 'Troll'
+    },
+    {
+        url: 'src/assets/Action/Luther.jpg',
+        title: 'Luther'
+    },
+    {
+        url: 'src/assets/Action/Mission Majnu.jpg',
+        title: 'Mission Majnu'
+    },
+    {
+        url: 'src/assets/Adventure/AKA.jpg',
+        title: 'AKA'
+    },
+    {
+        url: 'src/assets/Adventure/Blood & Gold.jpg',
+        title: 'Blood & Gold'
+    },
+    {
+        url: 'src/assets/Adventure/Enola Holmes.jpg',
+        title: 'Enola Holmes'
+    },
+    {
+        url: 'src/assets/Adventure/Heart of Stone.jpg',
+        title: 'Heart of Stone'
+    },
+    {
+        url: 'src/assets/Adventure/The Man of Toronto.jpg',
+        title: 'The Man of Toronto'
+    },
+    {
+        url: 'src/assets/Comedy/A Tourist\'s Guide to Love.jpg',
+        title: 'A Tourist\'s Guide to Love'
+    },
+    {
+        url: 'src/assets/Comedy/Ginny Weds Sunny.jpg',
+        title: 'Ginny Weds Sunny'
+    },
+    {
+        url: 'src/assets/Comedy/Love in the Villa.jpg',
+        title: 'Love in the Villa'
+    },
+    {
+        url: 'src/assets/Comedy/Love is in the Air.jpg',
+        title: 'Love is in the Air'
+    },
+    {
+        url: 'src/assets/Comedy/Your Place or Mine.jpg',
+        title: 'Your Place or Mine'
+    },
+    {
+        url: 'src/assets/Drama/Jamtara.jpg',
+        title: 'Jamtara'
+    },
+    {
+        url: 'src/assets/Drama/Khakee.jpg',
+        title: 'Khakee'
+    },
+    {
+        url: 'src/assets/Drama/Kohrra.jpg',
+        title: 'Kohrra'
+    },
+    {
+        url: 'src/assets/Drama/Narcos.jpg',
+        title: 'Narcos'
+    },
+    {
+        url: 'src/assets/Drama/The Last Kingdom.jpg',
+        title: 'The Last Kingdom'
+    },
+    {
+        url: 'src/assets/Romantic/Business Proposal.jpg',
+        title: 'Business Proposal'
+    },
+    {
+        url: 'src/assets/Romantic/Ginny & Georgia.jpg',
+        title: 'Ginny & Georgia'
+    },
+    {
+        url: 'src/assets/Romantic/Mr. Sunshine.jpg',
+        title: 'Mr. Sunshine'
+    },
+    {
+        url: 'src/assets/Romantic/Queen Charlotte.jpg',
+        title: 'Queen Charlotte'
+    },
+    {
+        url: 'src/assets/Romantic/The King.jpg',
+        title: 'The King'
+    }
+];
+
+export default MovieData;
