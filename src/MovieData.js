ba7556af48928c0b7236229e0951ecbd32aa1ce7
amankp12 <90567114+amankp12@users.noreply.github.com>
2024-03-02 12:04:28 +0530

    second commit

--- src/MovieData.js
+++ src/MovieData.js
@@ -1,102 +1,102 @@
 const MovieData = [
     {
-        url: 'src/assets/Action/Avatar.jpg',
+        url: '../assets/Action/Avatar.jpg',
         title: 'Avatar'
     },
     {
-        url: 'src/assets/Action/Extraction.jpg',
+        url: '../assets/Action/Extraction.jpg',
         title: 'Extraction'
     },
     {
-        url: 'src/assets/Action/Troll.jpg',
+        url: '../assets/Action/Troll.jpg',
         title: 'Troll'
     },
     {
-        url: 'src/assets/Action/Luther.jpg',
+        url: '../assets/Action/Luther.jpg',
         title: 'Luther'
     },
     {
-        url: 'src/assets/Action/Mission Majnu.jpg',
+        url: '../assets/Action/Mission Majnu.jpg',
         title: 'Mission Majnu'
     },
     {
-        url: 'src/assets/Adventure/AKA.jpg',
+        url: '../assets/Adventure/AKA.jpg',
         title: 'AKA'
     },
     {
-        url: 'src/assets/Adventure/Blood & Gold.jpg',
+        url: '../assets/Adventure/Blood & Gold.jpg',
         title: 'Blood & Gold'
     },
     {
-        url: 'src/assets/Adventure/Enola Holmes.jpg',
+        url: '../assets/Adventure/Enola Holmes.jpg',
         title: 'Enola Holmes'
     },
     {
-        url: 'src/assets/Adventure/Heart of Stone.jpg',
+        url: '../assets/Adventure/Heart of Stone.jpg',
         title: 'Heart of Stone'
     },
     {
-        url: 'src/assets/Adventure/The Man of Toronto.jpg',
+        url: '../assets/Adventure/The Man of Toronto.jpg',
         title: 'The Man of Toronto'
     },
     {
-        url: 'src/assets/Comedy/A Tourist\'s Guide to Love.jpg',
+        url: '../assets/Comedy/A Tourist\'s Guide to Love.jpg',
         title: 'A Tourist\'s Guide to Love'
     },
     {
-        url: 'src/assets/Comedy/Ginny Weds Sunny.jpg',
+        url: '../assets/Comedy/Ginny Weds Sunny.jpg',
         title: 'Ginny Weds Sunny'
     },
     {
-        url: 'src/assets/Comedy/Love in the Villa.jpg',
+        url: '../assets/Comedy/Love in the Villa.jpg',
         title: 'Love in the Villa'
     },
     {
-        url: 'src/assets/Comedy/Love is in the Air.jpg',
+        url: '../assets/Comedy/Love is in the Air.jpg',
         title: 'Love is in the Air'
     },
     {
-        url: 'src/assets/Comedy/Your Place or Mine.jpg',
+        url: '../assets/Comedy/Your Place or Mine.jpg',
         title: 'Your Place or Mine'
     },
     {
-        url: 'src/assets/Drama/Jamtara.jpg',
+        url: '../assets/Drama/Jamtara.jpg',
         title: 'Jamtara'
     },
     {
-        url: 'src/assets/Drama/Khakee.jpg',
+        url: '../assets/Drama/Khakee.jpg',
         title: 'Khakee'
     },
     {
-        url: 'src/assets/Drama/Kohrra.jpg',
+        url: '../assets/Drama/Kohrra.jpg',
         title: 'Kohrra'
     },
     {
-        url: 'src/assets/Drama/Narcos.jpg',
+        url: '../assets/Drama/Narcos.jpg',
         title: 'Narcos'
     },
     {
-        url: 'src/assets/Drama/The Last Kingdom.jpg',
+        url: '../assets/Drama/The Last Kingdom.jpg',
         title: 'The Last Kingdom'
     },
     {
-        url: 'src/assets/Romantic/Business Proposal.jpg',
+        url: '../assets/Romantic/Business Proposal.jpg',
         title: 'Business Proposal'
     },
     {
-        url: 'src/assets/Romantic/Ginny & Georgia.jpg',
+        url: '../assets/Romantic/Ginny & Georgia.jpg',
         title: 'Ginny & Georgia'
     },
     {
-        url: 'src/assets/Romantic/Mr. Sunshine.jpg',
+        url: '../assets/Romantic/Mr. Sunshine.jpg',
         title: 'Mr. Sunshine'
     },
     {
-        url: 'src/assets/Romantic/Queen Charlotte.jpg',
+        url: '../assets/Romantic/Queen Charlotte.jpg',
         title: 'Queen Charlotte'
     },
     {
-        url: 'src/assets/Romantic/The King.jpg',
+        url: '../assets/Romantic/The King.jpg',
         title: 'The King'
     }
 ];
